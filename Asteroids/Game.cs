921a0cc7303e365299b2de8876d09eb5fdd2fa28
Krosdir <pokemoon2698@gmail.com>
2018-08-12 16:37:21 +0700

    first commit

---
+++ Asteroids/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    class Game
+    {
+        static BufferedGraphicsContext context;
+        public static BufferedGraphics buffer;
+
+        static BaseObject[] objs;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+        }
+
+        public static void Init(Form form)
+        {
+            //Графическое устройство для вывода графики
+            Graphics grx;
+            //Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            context = BufferedGraphicsManager.Current;
+            //Создаем объект - поверхность рисования и связываем его с формой
+            grx = form.CreateGraphics();
+            //Запоминаем размеры формы
+            Width = form.Width;
+            Height = form.Height;
+            Load();
+            //Связываем буфер в памяти с графическим объектом
+            //Для того чтобы рисовать в буфере
+            buffer = context.Allocate(grx, new Rectangle(0,0,Width,Height));
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+            //Проверяем вывод графики
+            buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in objs)
+                obj.Draw();
+            buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in objs)
+                obj.Update();
+        }
+
+        public static void Load()
+        {
+            objs = new BaseObject[30];
+            for (int i = 0; i < objs.Length; i++)
+            {
+                objs[i] = new BaseObject(new Point(600, i*20), new Point(15-i, 15-i), new Size(20,20));
+            }
+        }
+    }
+}
